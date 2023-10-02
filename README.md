67238d0b5626c43d354bc765b8e97460e469f968
Leon Wu <lwgithub@gmail.com>
2023-10-01 20:10:28 -0700

    fix paths

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 This is an implementation of [guoyww/AnimateDiff](https://github.com/guoyww/animatediff/) as a Cog model. [Cog packages machine learning models as standard containers.](https://github.com/replicate/cog)
 
-Note that we want to be able use pre-downloaded weights as well, so that we don't have to download while building the docker container. This is nice because we can much more easily delete our existing docker container without having to do incredibly long re-build times, which we have to do afaict because cog doesn't have an easy way to cache things: https://github.com/replicate/cog/issues/1324
+Note that we want to be able use pre-downloaded weights as well, so that we don't have to download while building the docker container. This is nice because we can much more easily delete our existing docker container without having to do incredibly long re-build times, which we have to do afaict because cog doesn't have an easy way to un-cache things: https://github.com/replicate/cog/issues/1324. And, yes, you'll probably have to remove the existing Docker image becase git clone command doesn't change but the repo well might.
 
 First, download the pre-trained weights:
 
--- download_models.sh
+++ download_models.sh
@@ -1,9 +1,13 @@
 mkdir -p models
-wget -O models/mm_sd_v14.ckpt https://huggingface.co/guoyww/animatediff/resolve/main/mm_sd_v14.ckpt 
-wget -O models/mm_sd_v15.ckpt https://huggingface.co/guoyww/animatediff/resolve/main/mm_sd_v15.ckpt 
-wget -O models/mm_sd_v15_v2.ckpt https://huggingface.co/guoyww/animatediff/resolve/main/mm_sd_v15_v2.ckpt
-wget -O models/toonyou_beta3.safetensors https://civitai.com/api/download/models/78775
-wget -O models/lyriel_v16.safetensors https://civitai.com/api/download/models/72396
-wget -O models/rcnzCartoon3d_v10.safetensors https://civitai.com/api/download/models/71009
-wget -O models/majicmixRealistic_v5Preview.safetensors https://civitai.com/api/download/models/79068
-wget -O models/realisticVisionV40_v20Novae.safetensors https://civitai.com/api/download/models/29460
+mkdir -p models/Motion_Module
+mkdir -p models/DreamBooth_LoRA
+mkdir -p models/StableDiffusion
+git clone --branch fp16 https://huggingface.co/runwayml/stable-diffusion-v1-5 models/StableDiffusion/stable-diffusion-v1-5
+wget -O models/Motion_Module/mm_sd_v14.ckpt https://huggingface.co/guoyww/animatediff/resolve/main/mm_sd_v14.ckpt 
+wget -O models/Motion_Module/mm_sd_v15.ckpt https://huggingface.co/guoyww/animatediff/resolve/main/mm_sd_v15.ckpt 
+wget -O models/Motion_Module/mm_sd_v15_v2.ckpt https://huggingface.co/guoyww/animatediff/resolve/main/mm_sd_v15_v2.ckpt
+wget -O models/DreamBooth_LoRA/toonyou_beta3.safetensors https://civitai.com/api/download/models/78775
+wget -O models/DreamBooth_LoRA/lyriel_v16.safetensors https://civitai.com/api/download/models/72396
+wget -O models/DreamBooth_LoRA/rcnzCartoon3d_v10.safetensors https://civitai.com/api/download/models/71009
+wget -O models/DreamBooth_LoRA/majicmixRealistic_v5Preview.safetensors https://civitai.com/api/download/models/79068
+wget -O models/DreamBooth_LoRA/realisticVisionV40_v20Novae.safetensors https://civitai.com/api/download/models/29460
