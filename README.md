649211e98564a93c79aabec3680c5433541d6e25
Leon Wu <lwgithub@gmail.com>
2023-10-01 17:49:06 -0700

    redo model download logic

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 This is an implementation of [guoyww/AnimateDiff](https://github.com/guoyww/animatediff/) as a Cog model. [Cog packages machine learning models as standard containers.](https://github.com/replicate/cog)
 
+Note that we want to be able use pre-downloaded weights as well, so that we don't have to download while building the docker container. This is nice because we can much more easily delete our existing docker container without having to do incredibly long re-build times, which we have to do afaict because cog doesn't have an easy way to cache things: https://github.com/replicate/cog/issues/1324
+
 First, download the pre-trained weights:
 
     cog build -t animatediff
---
+++ download_models.sh
@@ -0,0 +1,9 @@
+mkdir -p models
+wget -O models/mm_sd_v14.ckpt https://huggingface.co/guoyww/animatediff/resolve/main/mm_sd_v14.ckpt 
+wget -O models/mm_sd_v15.ckpt https://huggingface.co/guoyww/animatediff/resolve/main/mm_sd_v15.ckpt 
+wget -O models/mm_sd_v15_v2.ckpt https://huggingface.co/guoyww/animatediff/resolve/main/mm_sd_v15_v2.ckpt
+wget -O models/toonyou_beta3.safetensors https://civitai.com/api/download/models/78775
+wget -O models/lyriel_v16.safetensors https://civitai.com/api/download/models/72396
+wget -O models/rcnzCartoon3d_v10.safetensors https://civitai.com/api/download/models/71009
+wget -O models/majicmixRealistic_v5Preview.safetensors https://civitai.com/api/download/models/79068
+wget -O models/realisticVisionV40_v20Novae.safetensors https://civitai.com/api/download/models/29460
